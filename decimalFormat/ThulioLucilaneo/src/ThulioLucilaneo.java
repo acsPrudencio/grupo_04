6cfd16deba304ff802af2a8d328935947346db0f
Thúlio Brandão <thulioasb@gmail.com>
2022-09-30 19:17:25 -0300

    adicionando informações

--- decimalFormat/ThulioLucilaneo/src/ThulioLucilaneo.java
+++ decimalFormat/ThulioLucilaneo/src/ThulioLucilaneo.java
@@ -1,3 +1,16 @@
+import java.text.DecimalFormat;
+
 public class ThulioLucilaneo {
-    
+    public static void main(String[] args) {
+        String padrao = "R$###,###.##";
+        DecimalFormat df = new DecimalFormat(padrao);
+
+        //df.applyPattern(padrao);
+        System.out.println(df.format(1234567890.123));
+
+        String padrao2 = "R$###,##0.00";
+        DecimalFormat dfs = new DecimalFormat(padrao2);
+
+        System.out.println(dfs.format(0.1));
+    }
 }
