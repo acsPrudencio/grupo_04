6be78ae828fd4025d3ff343f8d213969ba06f0c5
Lucilaneo Santos <lucilaneosantos@gmail.com>
2022-10-03 17:17:31 -0300

    amostra de percentual

--- decimalFormat/ThulioLucilaneo/src/ThulioLucilaneo.java
+++ decimalFormat/ThulioLucilaneo/src/ThulioLucilaneo.java
@@ -13,10 +13,17 @@ public class ThulioLucilaneo {
         df.applyLocalizedPattern(padrao2);
         System.out.println("SAIDA PADRAO 2 = "+ df.format(234567890.123));
 
-
+        //Aplicando real
         String padrao3 = "R$###.##0,00";
         DecimalFormat dfs = new DecimalFormat();
         dfs.applyLocalizedPattern(padrao3);
         System.out.println(dfs.format(0.1));
+
+        //Aplicando percentual
+        String padrao4 = "###.##00,00%";
+        DecimalFormat dfp = new DecimalFormat();
+        dfs.applyLocalizedPattern(padrao4);
+        System.out.println(dfs.format(0.1));
+
     }
 }
