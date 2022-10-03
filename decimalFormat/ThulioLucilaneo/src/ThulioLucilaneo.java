a5dce000703bcc74dc69897fe254c662faeadfa8
Lucilaneo Santos <lucilaneosantos@gmail.com>
2022-10-03 17:08:06 -0300

    Ajustando tipos de padrao

--- decimalFormat/ThulioLucilaneo/src/ThulioLucilaneo.java
+++ decimalFormat/ThulioLucilaneo/src/ThulioLucilaneo.java
@@ -2,15 +2,21 @@ import java.text.DecimalFormat;
 
 public class ThulioLucilaneo {
     public static void main(String[] args) {
-        String padrao = "R$###,###.##";
-        DecimalFormat df = new DecimalFormat(padrao);
 
-        //df.applyPattern(padrao);
-        System.out.println(df.format(1234567890.123));
+        //padrao utilizando - EN
+        String padrao1 = "###,###.##";
+        DecimalFormat df = new DecimalFormat(padrao1);
+        System.out.println("SAIDA PADRAO 1 = "+df.format(234567890.123));
 
-        String padrao2 = "R$###,##0.00";
-        DecimalFormat dfs = new DecimalFormat(padrao2);
+        //padrao utilizando localizacao - PT
+        String padrao2 = "###.###,##";
+        df.applyLocalizedPattern(padrao2);
+        System.out.println("SAIDA PADRAO 2 = "+ df.format(234567890.123));
 
+
+        String padrao3 = "R$###.##0,00";
+        DecimalFormat dfs = new DecimalFormat();
+        dfs.applyLocalizedPattern(padrao3);
         System.out.println(dfs.format(0.1));
     }
 }
