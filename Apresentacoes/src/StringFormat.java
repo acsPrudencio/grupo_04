67680d99ab7e66e26fe02392c306b96f706476e1
Daniel Moraes <96148567+SirDnx@users.noreply.github.com>
2022-10-04 14:00:52 -0300

    Alteração no código fonte usando a classe StringBuilder

--- Apresentacoes/src/StringFormat.java
+++ Apresentacoes/src/StringFormat.java
@@ -6,9 +6,17 @@ public class StringFormat {
 
             String nome = "Junior";
             double salario = 10562d;
+            String str1 = "";
+            String str2 = "";
 
         //Usando o método format.
             
-            System.out.print(String.format("Meu nome é %s e meu salário é R$ %,.2f", nome, salario));         
+            str1 = String.format("Meu nome é %s", nome);   
+            str2 = String.format(" e meu salário é R$ %,.2f", salario);
+
+            StringBuilder sb = new StringBuilder(str1);
+            
+            System.out.println(sb.append(str2));
+       
     }
 }
