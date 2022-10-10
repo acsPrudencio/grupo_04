724a1d0d480d03a96832026f2930e9f94727a2c3
Lucilaneo Santos <lucilaneosantos@gmail.com>
2022-10-10 17:02:14 -0300

    alterando nomeclatura dos testes

--- banco-digital/src/test/java/TestDepositoConta.java
+++ banco-digital/src/test/java/TestDepositoConta.java
@@ -6,13 +6,14 @@ import br.com.mjv.modelo.validations.ContaInexistente;
 import br.com.mjv.modelo.validations.SaldoInsuficiente;
 import br.com.mjv.modelo.validations.ValorDepositarInvalido;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TestDepositoConta {
-
+    @DisplayName("Realizar deposito ")
     @Test
     void testRealizarDeposito() throws ContaInexistente, ValorDepositarInvalido {
         Cliente Lucilaneo = new Cliente("Lucilaneo");
@@ -20,26 +21,39 @@ public class TestDepositoConta {
         Banco banco = new Banco();
         banco.incluir(cc);
         banco.deposito(cc,100);
-        assertEquals(4000, banco.saldo(cc), 0.001);
-
+        assertEquals(100, banco.saldo(cc), 0.001);
     }
 
-
+    @DisplayName("ValorDepositarInvalido - Deposito Negativo ")
     @Test
-    void testValorDepositarInvalidoException() throws ContaInexistente {
+    void testValorDepositarValorNegativo() throws ContaInexistente {
 
         Cliente Lucilaneo = new Cliente("Lucilaneo");
         ContaCorrente cc = new ContaCorrente(Lucilaneo);
         Banco banco = new Banco();
         banco.incluir(cc);
-        //Valor menor ou igual a zero atende(pass)
         ValorDepositarInvalido retorno = Assertions.assertThrows(ValorDepositarInvalido.class, () ->{
-           banco.deposito(cc,0);
+           banco.deposito(cc,-1);
                }, "Esperado exceção ValorDepositarInvalido");
         Assertions.assertEquals("O valor de depósito deve ser superior a zero.", retorno.toString());
 
 
 }
+    @DisplayName("ValorDepositarInvalido - Deposito com valor zero")
+    @Test
+    void testValorDepositarValorZero() throws ContaInexistente {
+
+        Cliente Lucilaneo = new Cliente("Lucilaneo");
+        ContaCorrente cc = new ContaCorrente(Lucilaneo);
+        Banco banco = new Banco();
+        banco.incluir(cc);
+        ValorDepositarInvalido retorno = Assertions.assertThrows(ValorDepositarInvalido.class, () ->{
+            banco.deposito(cc,0);
+        }, "Esperado exceção ValorDepositarInvalido");
+        Assertions.assertEquals("O valor de depósito deve ser superior a zero.", retorno.toString());
+
+
+    }
 
 
 
