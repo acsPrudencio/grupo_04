1c93ac6d66cb2264751b7bed4af2dabd1273a8b9
Evandrolds <evandro.lds.ds@outlook.com>
2022-10-15 19:38:37 -0300

    projeto inicial agua-luz-notificacao

---
+++ agua-luz-notificacao/src/main/java/br/com/mjv/controllers/Main.java
@@ -0,0 +1,22 @@
+package br.com.mjv.controllers;
+
+import br.com.mjv.model.Cliente;
+import br.com.mjv.model.Contrato;
+import br.com.mjv.services.ContratoService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Contrato c = new Contrato();
+        Cliente cl = new Cliente();
+        cl.setNome("Sandro");
+
+        ContratoService s = new ContratoService();
+
+        c.setCliente(cl);
+      
+        System.out.println(s.gerarContratoDeServico(cl, "us"));
+
+    }
+}
