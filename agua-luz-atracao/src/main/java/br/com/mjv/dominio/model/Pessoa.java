888e75301290103032361556f88659eeea6546eb
Antonio Carlos <acsprudencio@gmail.com>
2022-10-15 21:51:58 -0300

    Ajustes no projeto, e criacao da funcao para gravar as informacoes no arquivo

--- agua-luz-atracao/src/main/java/br/com/mjv/dominio/model/Pessoa.java
+++ agua-luz-atracao/src/main/java/br/com/mjv/dominio/model/Pessoa.java
@@ -1,10 +1,12 @@
-package br.com.mjv;
+package br.com.mjv.dominio.model;
 
 public class Pessoa {
     private String cpf;
     private String rg;
     private String nome;
     private String celular;
+
+    private Endereco endereco;
 
     public Pessoa(String cpf, String rg, String nome){
         this.cpf = cpf;
@@ -14,5 +16,13 @@ public class Pessoa {
 
     public void setNumeroCelular(String numero){
         this.celular = numero;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
     }
 }
